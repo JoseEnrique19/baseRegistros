a5c108e87774719245661d24273362c5174a6ff3
JoseEnrique19 <1715110333@utec-tgo.edu.mx>
2017-03-10 09:17:35 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$db_host = "y0nkiij6humroewt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+     $db_name = "ayr7fonkn1nockhg";
+     $db_user = "q8pcye2wm8ixc7j8";
+     $db_password = "ccr6wah0i64dkryx";
+    
+     $connection = mysql_connect(y0nkiij6humroewt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com, q8pcye2wm8ixc7j8, ccr6wah0i64dkryx);
+    
+mysql_select_db($db_name) or die("Error al seleccionar la base de datos:".mysql_error());
+    @mysql_query("SET NAMES 'utf8'");
+
+$sql_query = "SELECT * FROM productos;";
+$result = mysql_query($sql_query);
+$rows = array();
+while($r = mysql_fetch_assoc($result)) {
+  $rows[] = $r;
+}
+print json_encode($rows);
+?>
